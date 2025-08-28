c33015fa2e7de8e581632618edbec46177c981d3
Aaron <aroitma2@jh.edu>
2025-08-28 16:12:52 -0400

    Pre-processing comments and initial code

---
+++ preprocessing.m
@@ -0,0 +1,23 @@
+function filtered_choices = preprocessing(doctor_choices, capacities)
+    % This function serves to process the input data doctors use to rank
+    % hospitals, sanitizing inputs so that no errors are encountered upon
+    % optimization
+    % Inputs: 
+    % -  doctor_choices: the choices submitted by doctors (XxN array
+    %    containing up to N choices of hospitals submitted by X number of 
+    %    doctors on where they would like to work)
+    % -  capacities: 1xN array of N hospitals with values corresponding to
+    %    the capacity of each hospital 
+    % Outputs: 
+    % - filtered_choices: Sanitized data with information on where
+    %   each doctor would prefer to work
+    % - hospital_capacity: how many doctors can work at each hospital
+
+% Filter out empty entries
+filtered_choices = doctor_choices(any(doctor_choices,2), :)
+% Process incomplete data (less than 5 entries)
+
+% Truncate overcomplete data (more than 5 entries) -- this will work for
+% cells only
+filtered_choices = filtered_choices(cellfun(@length, filtered_choices) <= 5)
+% Process bad data (same hospital twice)
