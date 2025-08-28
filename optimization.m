ff31bcd6910932505f448fb3352bba4994497ffd
Joeywroberts <joeywroberts12@gmail.com>
2025-08-28 15:57:05 -0400

    Starting ideas for optimization.

---
+++ optimization.m
@@ -0,0 +1,5 @@
+% Piority should be given at random to people.
+% The optimization method that we could use is Hungarian Algorithm 
+% to maximize global satifaction for a group of roughtly 1000 doctors.
+% Unmatched doctors are held to the end of assigment and then assigned to
+% the hopital with the most need (least number of doctors).
