dce7a1f628d613696674998725797d60dd3dc25b
Aaron <aroitma2@jh.edu>
2025-09-02 14:29:13 -0400

    Generate test data for Hungarian algorithm optimization

---
+++ test.m
@@ -0,0 +1,32 @@
+function capacity = test()
+% Initialize parameters for test data generation
+    numDoctors = 10; 
+    numHospitals = 5; 
+    capacity = ceil(numDoctors/numHospitals);
+    test_data = generate_test_data(numDoctors, numHospitals);
+    test_data = hungarian_test_data(capacity, test_data, numDoctors);
+end
+
+function test_data = generate_test_data(num_doctors, num_hospitals)
+    % This function serves to generate test data based on the number of
+    % doctors and number of hospital. We assume that each doctor can list
+    % up to N choices given N hospitals
+    % Inputs: 
+    % -  num_doctors: The number of doctors
+    % -  num_hospitals: The number of hospitals
+    % Outputs: 
+    % - test_data: A XxN array for X doctors and N hospitals with
+    %   randomized selections of hospitals for each doctor
+
+    % Generate random hospital choices for each doctor
+    test_data = zeros(num_doctors, num_hospitals);
+    for i = 1:num_doctors
+        test_data(i, :) = randperm(num_hospitals, num_hospitals);
+    end
+end
+
+function test_data = hungarian_test_data(capacity, test_data, numDoctors)
+    test_data = repmat(test_data, capacity);
+    test_data = test_data(1:numDoctors, :);
+end
+    
