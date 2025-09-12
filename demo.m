859bc1749ddc3d88091c55cfdfdc2afa6fca73bc
Joeywroberts <joeywroberts12@gmail.com>
2025-09-12 16:47:45 -0400

    Set up bones for the demo and put a sample hardcoded data

--- demo.m
+++ demo.m
@@ -1,6 +1,23 @@
-[capacity, cost_mat] = test();
+% Generated test data with 5 hositpals and 10 doctors
+[capacities, doctor_choices] = test(5, 10);
 
-[assignment, totalCost] = optimization(cost_mat, capacity);
+% Some hard coded data
+% doctor_choices = {[4,1,1,3,6],
+%                   [1,5,3,2,4],
+%                   [2,3,5,1],
+%                   [3,5,4,2,2],
+%                   [1,1,1,3],
+%                   [3,2,5,4,1],
+%                   [2,3,4,1,5,6],
+%                   [2,3,5,1,1,4],
+%                   [1,4,3],
+%                   [1,5,6,8,2,3,4]};
+% capacities = [2,1,2,3,4];
+
+% Process the input data to create a matrix to put into the hungarian
+hungarian_mat = preprocessing(doctor_choices, capacities);
+
+[assignment, totalCost] = optimization(hungarian_mat, capacities);
 
 % Display results
 disp(totalCost);
